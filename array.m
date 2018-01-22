a7eeb415bd43d657aa5aca6b479aab2812c9ed8d
ammar903@gmail.com <ammar903@gmail.com>
2018-01-22 13:40:01 +0000

    more stuff

--- array.m
+++ array.m
@@ -3,10 +3,10 @@ d = lambda/4;
 phi = pi/2;
 k = 2*pi/lambda
 
-theta = 0:0.01:2*pi
+theta = 0:0.01:2*pi;
 
 
-rho = 2*cos(k * d/2 * cos(theta) - phi/2);
+rho = 2*cos(k * d/2 * cos(theta) + phi/2);
 
 polar(theta, rho);
 polarplot(theta,rho);
